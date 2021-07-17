f021bd76e43b5601b85284b41db0a50cf9958a1a
Oleg Samarin <osamarin@openwaygroup.com>
2021-07-17 20:24:54 +0300

    First addition

---
+++ debian/cmake/cmake_admin/MinGWCrossCompile.cmake
@@ -0,0 +1,34 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Windows)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+SET(MINGW_PREFIX /usr/${MINGW_TARGET}/)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH	${MINGW_PREFIX})
+SET(CMAKE_INSTALL_PREFIX	${MINGW_PREFIX})
+SET(CMAKE_INSTALL_LIBDIR	${MINGW_PREFIX}/lib)
+
+SET(MINGW_TOOL_PREFIX		/usr/bin/${MINGW_TARGET}-)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER		${MINGW_TOOL_PREFIX}gcc)
+SET(CMAKE_RC_COMPILER		${MINGW_TOOL_PREFIX}gcc)
+SET(CMAKE_CXX_COMPILER		${MINGW_TOOL_PREFIX}g++)
+
+# specify location of some tools
+SET(STRIP					${MINGW_TOOL_PREFIX}strip)
+SET(WINDRES					${MINGW_TOOL_PREFIX}windres)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+SET(CMAKE_INCLUDE_PATH ${MINGW_PREFIX}/include)
+
+INCLUDE_DIRECTORIES(${MINGW_PREFIX}/include)
+LINK_DIRECTORIES(${MINGW_PREFIX}/lib)
+
