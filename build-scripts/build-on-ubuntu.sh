3f72e730794a61cf539e3a1e701af120009296a4
Oleg Samarin <osamarin@openwaygroup.com>
2021-07-17 21:14:05 +0300

    Build scripts

---
+++ build-scripts/build-on-ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+SRC_DIR=$(readlink -f $(dirname $0)/..)
+echo SRC_DIR=$SRC_DIR
+
+#DEBIAN_PKG_NAME=portaudio-mingw
+#VERSION=${1:-19.600}
+#BUILD_VERSION=${2:-3.os}
+
+BUILD_DIR=`pwd`/build
+
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR
+rm -rf *
+
+cp -r $SRC_DIR/* ./
+
+dpkg-buildpackage -us -uc
+
+popd
+
---
+++ build-scripts/prepare-ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get update
+#sudo DEBIAN_FRONTEND=noninteractive apt-get install -y debhelper g++-mingw-w64 cmake wget file mc build-essential dh-make
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y wget unzip debhelper g++-mingw-w64 cmake
+
+# download and install ASIO sdk
+if [ ! -d /usr/local/asio-sdk ]; then
+	DL_DIR=`mktemp -d -t asio.XXX`
+	wget -O $DL_DIR/asiosdk.zip https://www.steinberg.net/asiosdk
+	sudo unzip -o $DL_DIR/asiosdk.zip -d /usr/local/
+	rm -rf $DL_DIR
+	SDK_DIR=`ls -1d /usr/local/asiosdk* | tail -1`
+	sudo ln -sf `basename $SDK_DIR` /usr/local/asio-sdk
+fi
+
